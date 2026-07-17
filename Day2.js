763aee98af93158047716779a5222f3fa9fc34f4
gokul.s <kgokula48@gmail.com>
2026-07-17 23:59:48 +0530

    day 2 done

---
+++ Day2.js
@@ -0,0 +1,20 @@
+// // console.log("Hey I am JS");
+// // console.log(a);
+// // console.log(b);
+// var a = 10;
+// let b = 20;
+// // const c = 30;
+
+// console.log(multiply(a, b));
+
+// function multiply(x, y) {
+//   // let b = 20;
+//   // var a = 100;
+//   // console.log(a);
+//   var result = x * y;
+//   return result;
+// }
+
+// // var answer = multiply(a, b);
+
+// // console.log("The answer is: " + answer);
